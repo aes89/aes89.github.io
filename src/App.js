fac77763abd464eeb58cbeff73c05a2ae1ffe95b
aes89 <adrienne.e.smith@outlook.com>
2025-07-16 22:47:35 +1000

    Nav bar!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import './App.css'
+import Icon from '@mui/icons-material/Menu'
 
 function App() {
-  return (
-    <div className="app-container">
-      Stuff coming soon :)
-    </div>
-  );
+    function showNavMenu() {
+        var x = document.getElementById('menuLinks')
+        if (x.style.display === 'flex') {
+            x.style.display = 'none'
+        } else {
+            x.style.display = 'flex'
+        }
+    }
+    return (
+        <div className="app-container">
+            <nav>
+                <Icon
+                    className="MenuIcon"
+                    onClick={() => showNavMenu()}
+                />
+                <h1 className="dancing-script">Doodles</h1>
+            </nav>
+            <div id="menuLinks">
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+            <p>Stuff coming soon</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
