836fcde53b625055ef2dedbddbe9a86d12535461
aes89 <adrienne.e.smith@outlook.com>
2025-07-18 21:36:01 +1000

    Menu symbol conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import './App.css'
-import Icon from '@mui/icons-material/Menu'
+import { Menu, MenuOpen } from '@mui/icons-material'
+import { useState } from 'react'
 
 function App() {
+    const [isMenuOpen, setisMenuOpen] = useState(false)
+    console.log(isMenuOpen)
+
     function showNavMenu() {
         var x = document.getElementById('menuLinks')
         if (x.style.display === 'flex') {
             x.style.display = 'none'
+            setisMenuOpen(false)
         } else {
             x.style.display = 'flex'
+            setisMenuOpen(true)
         }
     }
+
     return (
         <div className="app-container">
             <nav>
-                <Icon
-                    className="MenuIcon"
-                    onClick={() => showNavMenu()}
-                />
+                {isMenuOpen ? (
+                    <MenuOpen
+                        className="MenuIcon"
+                        onClick={() => showNavMenu()}
+                    />
+                ) : (
+                    <Menu className="MenuIcon" onClick={() => showNavMenu()} />
+                )}
                 <h1 className="dancing-script">Doodles</h1>
             </nav>
             <div id="menuLinks">
