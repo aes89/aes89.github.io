4bd448d022095a06f8c48b84db713832dba09d08
aes89 <adrienne.e.smith@outlook.com>
2025-07-20 17:27:12 +1000

    Refactor file structure

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import './App.css'
-import { Menu, MenuOpen } from '@mui/icons-material'
-import { useState } from 'react'
+import NavBar from './nav'
+
 
 function App() {
-    const [isMenuOpen, setisMenuOpen] = useState(false)
-    console.log(isMenuOpen)
-
-    function showNavMenu() {
-        var x = document.getElementById('menuLinks')
-        if (x.style.display === 'flex') {
-            x.style.display = 'none'
-            setisMenuOpen(false)
-        } else {
-            x.style.display = 'flex'
-            setisMenuOpen(true)
-        }
-    }
-
     return (
         <div className="app-container">
-            <nav>
-                {isMenuOpen ? (
-                    <MenuOpen
-                        className="MenuIcon"
-                        onClick={() => showNavMenu()}
-                    />
-                ) : (
-                    <Menu className="MenuIcon" onClick={() => showNavMenu()} />
-                )}
-                <h1 className="dancing-script">Doodles</h1>
-            </nav>
-            <div id="menuLinks">
-                <a href="#news">About</a>
-                <a href="#contact">Gallery</a>
-            </div>
+            <NavBar />
             <p>Stuff coming soon</p>
         </div>
     )
